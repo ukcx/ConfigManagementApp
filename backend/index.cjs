3fb70a6c8602ec5eac65b0897be2b45306f9f54a
Uğur Kağan Çakır <80484731+ukcx@users.noreply.github.com>
2024-01-25 19:12:08 +0300

    fix

--- backend/index.cjs
+++ backend/index.cjs
@@ -317,8 +317,8 @@ app.get('/parameters/:key/:cc?', validateFirebaseToken, async (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-//module.exports = app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = app;
